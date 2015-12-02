8f64ae9050ea0f438f8fc633ba7b68e0c10fc10a
vbrugada <victoria.brugada@neuro.fchampalimaud.org>
2015-12-02 18:52:31 +0000

    Handedness ok!

--- loaddata.m
+++ loaddata.m
@@ -1,28 +1,72 @@
 %% get the data from the .csv files
 
 idData = fopen('test1v_responses.csv');
-data = textscan(idData, '%s %s %s');
+data = textscan(idData, '%q %q %q', 'Delimiter', ',');
 fclose(idData);
 
 datamatrix = [data{2} data{3}];
 datamatrix = sortrows(datamatrix);
 
+%% Demographics
+gender = cell2mat(datamatrix(strmatch('Demographics.Gender', ...
+    datamatrix(:, 1)),2));
+gender = gender(1);
+
+age = cell2mat(datamatrix(strmatch('Demographics.Age', ...
+    datamatrix(:, 1)),2));
+
+%% Handedness --
+
+idStimuliHand = fopen('Handedness_Stimuli.csv');
+stimHandedness = textscan(idStimuliHand, '%q %q %q', 'Delimiter', ';');
+fclose(idStimuliHand);
+
+dataHandedness = [stimHandedness{1}, stimHandedness{3}];
+dataHandedness = sortrows(dataHandedness, 2);
+ind_questions = dataHandedness(:, 2);
+
+ind_ansHandedness = strmatch('Handedness', datamatrix(:,1)); %get indices Handedness
+
+ansHandedness = datamatrix(ind_ansHandedness, :);
+
+% variables for the for each hand
+ind_right = 0;
+ind_left = 0;
+
+for i_hand = 1:numel(ind_questions)
+    wordToFind = ind_questions(i_hand);
+    wordToFind = cell2mat(wordToFind);
+    index = find(cellfun('length',regexp(ansHandedness,wordToFind)) == 1);
+    answerHand = ansHandedness(index, 2);
+    if numel(answerHand) > 1
+        if strcmp(answerHand(1), 'right')
+            ind_right = ind_right + 1;
+        elseif strcmp(answerHand(1), 'left')
+            ind_left = ind_left + 1;
+        end
+    else
+        if strcmp (answerHand, 'right')
+            ind_right = ind_right + 2;
+        elseif strcmp(answerHand, 'left')
+            ind_left = ind_left + 2;
+        elseif strcmp(answerHand, 'none')
+            ind_right = ind_right + 1;
+            ind_left = ind_left + 1;
+        end 
+    end
+end
+
+lateralityIndex = ((ind_right - ind_left)/(ind_right + ind_left));
+
+%% NEOPI --
+
+% get the correct index
 numvect = [1 10 100:109 11 110:119 12 120:129 13 130:139 14 140:149 ...
     15 150:159 16 160:169 17 170:179 18 180:189 19 190:199 2 20 200:209 ...
     21 210:219 22 220:229 23 230:239 24 240 25:29 3 30:39 4 40:49 5 ...
     50:59 6 60:69 7 70:79 8 80:89 9 90:99]';
 
 [sorted, indsort] = sort(numvect);
-%% Demographics
-gender = cell2mat(datamatrix(strmatch('Demographics.Gender', ...
-    datamatrix(:, 1)),2));
-gender = gender(2);
-
-age = cell2mat(datamatrix(strmatch('Demographics.Age', ...
-    datamatrix(:, 1)),2));
-age = str2double(age(2:3));
-
-%% NEOPI --
 
 posNEOPI = strmatch('NEOPI', datamatrix(:,1)); % get indices of NEOPI
 posNEOPI_Page = strmatch('NEOPI.Page', datamatrix(:, 1)); % get indices NEOPI.Page
@@ -51,8 +95,6 @@ end
 
 [Tvalues_facets, Tvalues_domains] = findTvalues (vectFacets, vectDomains, gender);
 
-%% Handedness --
-posHandedness = strmatch('Handedness', datamatrix(:,1)); %get indices Handedness
 
 
 
@@ -125,16 +167,72 @@ for i = 1:length(ansRMET)
 
 resultRMET = sum(cmpAns);
 
-%% test
-
-
 %% MET --
+
+% get the answers of the participant
+posMET = strmatch('MET', datamatrix (:, 1)); 
+posMET_Practice = strmatch('MET.practise', datamatrix (:, 1));
+% posRMET_DefinUnd = strmatch('RMET.Definition', datamatrix (:, 1));
+% posRMET_Defin = strmatch('RMET.Definition.undefined', datamatrix (:, 1));
+posMET_Page = strmatch('MET.Page', datamatrix (:, 1));
+
+% % remove the rows that contain 'RMET.Page', 'RMET.Definition', and
+% % 'RMET.Practice'
+% allRMET = datamatrix(posRMET,:);
+% 
+for i = 1:numel(posMET_Page)
+   posMET(posMET == posMET_Page(i)) = [];
+end
+% 
+% for j = 1:numel(posRMET_Defin)
+%     posRMET(posRMET == posRMET_Defin(i)) = [];
+% end
+% 
+
+for k = 1:numel(posMET_Practice)
+    posMET(posMET == posMET_Practice(i)) = [];
+end
+% 
+% for l = 1:numel(posRMET_DefinUnd)
+%     posRMET(posRMET == posRMET_DefinUnd(i)) = [];
+% end
+% 
+ansMET = datamatrix(posMET, 1:2);
+
+
+idStimuliMET = fopen('MET_Stimuli.csv');
+stimuliMET = textscan(idStimuliMET, '%s %s %s %s %s %s %s %s', ...
+    'Delimiter', ';');
+fclose(idStimuliMET);
+
+MET = {};
+
+for i = 1:size(stimuliMET{1}, 1)
+    % Extract stimulus number from image name
+    tokens = regexpi(stimuliMET{2}(i), '([0-9]*)', 'tokens');
+    
+    if(isempty(tokens{1})), continue; end;
+    
+    stimulusNr = str2double(tokens{1}{1});
+    
+    % Extract data for stimulus
+    field = ['MET.' num2str(stimulusNr) '.A,'];
+    answerA = data{3}(strcmp(data{2}, field));
+
+    field = ['MET.' num2str(stimulusNr) '.B,'];
+    answerB = data{3}(strcmp(data{2}, field));
+    
+    % Extract correct answer    
+    correctAnswer = stimuliMET{8}(i);
+    
+    MET(i, 1:4) = [stimulusNr answerA answerB correctAnswer];
+end
+
+
+
+
 
 
 %% Circles --
 
 %% LOCS --
-
-
-
-
