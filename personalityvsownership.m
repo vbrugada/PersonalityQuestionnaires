65b64fddac166dd7744bf1e35404b0be1e447ac2
vbrugada <victoria.brugada@neuro.fchampalimaud.org>
2016-02-17 16:23:27 +0000

    ?

--- personalityvsownership.m
+++ personalityvsownership.m
@@ -40,6 +40,8 @@ for i = 1:size(personalitystruc, 2)
 end
 
 
+
+
 %% NEOPI vs ownership
 fig1 = figure;
 
@@ -58,12 +60,13 @@ namedomains = {'Neuroticism', 'Extraversion', 'Openness', ...
 
 for i_neopif = 1:size(NEOPIfacetsvalues, 2)
     
-    [coefNEOPIf, pvalNEOPIf] = corrcoef(ownparticipants, NEOPIfacetsvalues(:, i_neopif));
+    [coefNEOPIf, pvalNEOPIf] = corrcoef(ownparticipants, ...
+        NEOPIfacetsvalues(:, i_neopif));
     subplot(5, 7, vectfacets(i_neopif))
     plot(ownparticipants, NEOPIfacetsvalues(:, i_neopif), '.b', 'MarkerSize', 20);
     
-    set (gca, 'XTick', 1:7);
-    set (gca, 'YTick', 20:10:80);
+    set (gca, 'XTick', []);
+    set (gca, 'YTick', []);
     
     formatcorrplots ([0.5 7.5], [19.5 80.5], namefacets(i_neopif), '' , ...
         '', [0, 8], [20, 80], [20, 80], [40, 40], [4, 4], [20, 80]);
@@ -71,6 +74,7 @@ for i_neopif = 1:size(NEOPIfacetsvalues, 2)
     
     if i_neopif == 25 || i_neopif == 26 || i_neopif == 27 || i_neopif == 28 ...
             || i_neopif == 29 || i_neopif == 30
+        set (gca, 'XTick', 1:7);
         xlabel('Ownership', 'FontSize', 12, 'FontWeight', 'bold');
     end
     
@@ -84,7 +88,7 @@ for i_neopid = 1:size(NEOPIdomainsval, 2)
     subplot(5, 7, vectdomain(i_neopid))
     
     plot(ownparticipants, NEOPIdomainsval(:, i_neopid), '.r', 'MarkerSize', 20);
-    set (gca, 'XTick', 1:7);
+    set (gca, 'XTick', []);
     set (gca, 'YTick', 20:10:80);
     
     formatcorrplots ([0.5 7.5], [19.5 80.5], namedomains(i_neopid), '' , ...
@@ -93,28 +97,15 @@ for i_neopid = 1:size(NEOPIdomainsval, 2)
      ylabel('NEOPI T-values', 'FontSize', 14, 'FontWeight', 'bold');
     
     if i_neopid == 5
+        set (gca, 'XTick', 1:7);
         xlabel('Ownership', 'FontSize', 12, 'FontWeight', 'bold');
     end
     
 end
 
 set(fig1,'units','normalized', 'Position', [0 0 0.95 0.95])
-
 %% NEOPI(normed) vs ownership
 fig2 = figure;
-
-vectfacets = [2, 3, 4, 5, 6, 7, 9, 10, 11, 12, 13, 14, 16, 17, 18, 19, ...
-    20, 21, 23, 24, 25, 26, 27, 28, 30, 31, 32, 33, 34, 35];
-namefacets = {'Anxiety', 'Angry Hostility', 'Depression', ...
-    'Self-Consciousness', 'Impulsiveness', 'Vulnerability', 'Warmth', ...
-    'Gregariousness', 'Assertiveness', 'Activity', 'Excitement-seeking', ...
-    'Positive Emotions', 'Fantasy', 'Aesthetics', 'Feelings', 'Actions', ...
-    'Ideas', 'Values', 'Trust', 'Straightforwardness', 'Altruism', ...
-    'Compliance', 'Modesty', 'Tender-Mindedness', 'Competence', 'Order', ...
-    'Dutifulness', 'Achievement Striving', 'Self-Discipine', 'Deliberation'};
-vectdomain = [1, 8, 15, 22, 29];
-namedomains = {'Neuroticism', 'Extraversion', 'Openness', ...
-    'Agreeableness', 'Conscientiousness'};
 
 for i_neopif = 1:size(NEOPIfacetsvalues, 2)
     
@@ -146,7 +137,7 @@ for i_neopid = 1:size(NEOPIdomainsval, 2)
     subplot(5, 7, vectdomain(i_neopid))
     
     plot(ownparticipants, NEOPIfacets_norm(:, i_neopid), '.r', 'MarkerSize', 20);
-    set (gca, 'XTick', 1:7);
+    set (gca, 'XTick', []);
     set (gca, 'YTick', 1:1:5);
     set (gca, 'YTickLabel', {'Very Low', 'Low', 'Average', 'High', 'Very High'});
     
@@ -157,6 +148,7 @@ for i_neopid = 1:size(NEOPIdomainsval, 2)
      ylabel('NEOPI T-values', 'FontSize', 14, 'FontWeight', 'bold');
     
     if i_neopid == 5
+        set (gca, 'XTick', 1:7);
         xlabel('Ownership', 'FontSize', 12, 'FontWeight', 'bold');
     end
     
@@ -168,7 +160,7 @@ set(fig2,'units','normalized', 'Position', [0 0 0.95 0.95])
 %% RMET vs ownership
 [coefRMET, pvalRMET] = corrcoef(ownparticipants, RMETvalues);
 
-fig2 = figure;
+fig3 = figure;
 plot(ownparticipants, RMETvalues,'.b', 'MarkerSize', 20);
 
 text(1, 33, strcat('R = ',num2str(coefRMET(1, 2))), 'FontSize', 11);
@@ -184,12 +176,12 @@ formatcorrplots ([0.5 7.5], [0.5 36.5], ...
 set (gca, 'XTick', 1:7);
 set (gca, 'YTick', 0:5:36);
 
-set(fig2,'units','normalized', 'Position', [0.1 0.1 0.45 0.7])
+set(fig3,'units','normalized', 'Position', [0.1 0.1 0.45 0.7])
 
 %% MET vs ownership
 [coefMET, pvalMET] = corrcoef(ownparticipants, METvalues);
 
-fig3 = figure;
+fig4 = figure;
 plot(ownparticipants, METvalues,'.b', 'MarkerSize', 20);
 
 text(1, 36, strcat('R = ',num2str(coefMET(1, 2))), 'FontSize', 11);
@@ -202,12 +194,12 @@ formatcorrplots ([0.5 7.5], [0.5 40.5], ...
 set (gca, 'XTick', 1:7, 'FontSize', 11);
 set (gca, 'YTick', 0:5:40, 'FontSize', 11);
 
-set(fig3,'units','normalized', 'Position', [0.1 0.1 0.45 0.7]);
+set(fig4,'units','normalized', 'Position', [0.1 0.1 0.45 0.7]);
 
 %% LOCS vs ownership
 [coefLOCS, pvalLOCS] = corrcoef (ownparticipants, LOCSex_val);
 
-fig4 = figure;
+fig5 = figure;
 plot(ownparticipants, LOCSex_val,'.b', 'MarkerSize', 20);
 
 text(1, 96, strcat('R = ',num2str(coefLOCS(1, 2))), 'FontSize', 11);
@@ -220,12 +212,12 @@ formatcorrplots ([0.5 7.5], [0.5 100.5], 'Ownership - LOCS External', ...
 set (gca, 'XTick', 1:7, 'FontSize', 11);
 set (gca, 'YTick', 0:10:100, 'FontSize', 11);
 
-set(fig4,'units','normalized', 'Position', [0.2 0.2 0.45 0.7]);
+set(fig5,'units','normalized', 'Position', [0.2 0.2 0.45 0.7]);
 
 %% MET RMET
 [coefMET_RMET, pvalMET_RMET] = corrcoef (RMETvalues, METvalues);
 
-fig5 = figure;
+fig6 = figure;
 plot(RMETvalues, METvalues,'.b', 'MarkerSize', 20);
 
 text(3, 37, strcat('R = ',num2str(coefMET_RMET(1, 2))));
@@ -238,4 +230,4 @@ formatcorrplots ([0.5 36.5], [0.5 40.5], 'RMET and MET Cognitive', ...
     'Individual values for RMET', 'Individual values for MET (Cognitive)', ...
     [0, 36], [0, 40], [0, 40], [20, 20], [18, 18], [0, 40]);
 
-set(fig5,'units','normalized', 'Position', [0.2 0.2 0.45 0.7]);
+set(fig6,'units','normalized', 'Position', [0.2 0.2 0.45 0.7]);
